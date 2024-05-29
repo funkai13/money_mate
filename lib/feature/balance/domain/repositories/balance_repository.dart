23b2387dcdf93c22326924d9d38b8b4a456a6312
funkai13 <evinssonmedrano@gmail.com>
2024-05-29 00:08:40 -0600

    Create balance_repository.dart

---
+++ lib/feature/balance/domain/repositories/balance_repository.dart
@@ -0,0 +1,10 @@
+import '../entity/balance.dart';
+
+abstract interface class BalanceRepository {
+  Future<AccountBalance> createAccountBalance({
+    required String title,
+    required double balance,
+    required DateTime creationDate,
+    required DateTime updateDate,
+  });
+}
