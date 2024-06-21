ef93af993acd28d3faff3b51c988aaa8da04ce23
funkai13 <evinssonmedrano@gmail.com>
2024-06-20 20:14:12 -0600

    Update create_account_balance_dialog.dart

--- lib/feature/balance/presenter/screens/widgets/create_account_balance_dialog.dart
+++ lib/feature/balance/presenter/screens/widgets/create_account_balance_dialog.dart
@@ -8,7 +8,15 @@ import 'package:money_mate/core/mobile_design_system/shared/widgets/dialog_form_
 import '../../controller/balance_controller.dart';
 
 class CreateAccountBalanceDialog extends ConsumerStatefulWidget {
-  const CreateAccountBalanceDialog({super.key});
+  final bool isEditing;
+  final String? title;
+  final double? balance;
+  const CreateAccountBalanceDialog({
+    super.key,
+    required this.isEditing,
+    required this.title,
+    required this.balance,
+  });
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() =>
@@ -17,17 +25,60 @@ class CreateAccountBalanceDialog extends ConsumerStatefulWidget {
 
 class _CreateAccountBalanceDialog
     extends ConsumerState<CreateAccountBalanceDialog> {
+  Future<void> _processAccountBalance() async {
+    final isEditing = widget.isEditing;
+    if (isEditing) {
+      await _updateAccountBalance();
+    } else {
+      await _createAccountBalance();
+    }
+  }
+
   Future<void> _createAccountBalance() async {
     if (!formKey.currentState!.validate()) return;
-    ref.read(balanceControllerProvider.notifier).createAccountBalance(
-          title: _accountNameController.text,
+    try {
+      await ref.read(balanceControllerProvider.notifier).createAccountBalance(
+            title: _accountNameController.text,
+            balance: double.parse(_balanceController.text),
+            creationDate: DateTime.now(),
+            updateDate: DateTime.now(),
+          );
+      if (mounted) {
+        ref.refresh(balanceControllerProvider);
+        Navigator.of(context).pop();
+        Future.delayed(const Duration(milliseconds: 100)).then((_) {
+          context.go('/home');
+        });
+      }
+    } catch (e) {
+      print('Error creating account: $e');
+      // Manejo adicional de errores si es necesario
+    }
+  }
+
+  Future<void> _updateAccountBalance() async {
+    if (!formKey.currentState!.validate()) return;
+    await ref.read(balanceControllerProvider.notifier).updateOne(
+          currentTitle: widget.title ?? '',
+          newTitle: _accountNameController.text,
           balance: double.parse(_balanceController.text),
-          creationDate: DateTime.now(),
-          updateDate: DateTime.now(),
         );
     if (mounted) {
-      Navigator.of(context).pop();
       ref.refresh(balanceControllerProvider);
+      Navigator.of(context).pop();
+      Future.delayed(const Duration(milliseconds: 100)).then((_) {
+        context.go('/home');
+      });
+    }
+  }
+
+  Future<void> _deleteAccountBalance() async {
+    await ref
+        .read(balanceControllerProvider.notifier)
+        .delete(currentTitle: widget.title ?? '');
+    if (mounted) {
+      ref.refresh(balanceControllerProvider);
+      Navigator.of(context).pop();
       Future.delayed(const Duration(milliseconds: 100)).then((_) {
         context.go('/home');
       });
@@ -37,6 +88,13 @@ class _CreateAccountBalanceDialog
   late final formKey = GlobalKey<FormState>();
   final _accountNameController = TextEditingController();
   final _balanceController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _accountNameController.text = widget.title ?? '';
+    _balanceController.text = widget.balance?.toString() ?? '';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +108,6 @@ class _CreateAccountBalanceDialog
             padding: const EdgeInsets.all(8.0),
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.white,
                 borderRadius: BorderRadius.circular(15),
               ),
               child: Row(
@@ -84,7 +141,10 @@ class _CreateAccountBalanceDialog
             height: 10,
           ),
           const Padding(
-            padding: EdgeInsets.all(8.0),
+            padding: EdgeInsets.only(
+              top: 12,
+              left: 16,
+            ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -138,7 +198,8 @@ class _CreateAccountBalanceDialog
             child: SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: _createAccountBalance,
+                style: const ButtonStyle(),
+                onPressed: _processAccountBalance,
                 child: const Text(
                   'Guardar',
                   style: TextStyle(color: Colors.black),
@@ -147,6 +208,24 @@ class _CreateAccountBalanceDialog
             ),
           ),
           const SizedBox(height: 20),
+          if (widget.isEditing)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.red),
+                  ),
+                  onPressed: _deleteAccountBalance,
+                  child: const Text(
+                    'Eliminar',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            )
         ],
       ),
     );
