9280f143f5e089e3c67b9d1938cadf53e2c894d4
funkai13 <evinssonmedrano@gmail.com>
2024-07-18 22:01:10 -0600

    Update transactions_provider.dart

--- lib/feature/Transactions/domain/provider/transactions_provider.dart
+++ lib/feature/Transactions/domain/provider/transactions_provider.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:money_mate/feature/Transactions/data/services/income_service.dart';
 import 'package:money_mate/feature/Transactions/domain/repositories/income_repository.dart';
 
 final incomeServiceProvider = Provider<IncomeRepository>(
-    (_) => IncomeService(FirebaseFirestore.instance));
+    (_) => IncomeService(FirebaseFirestore.instance, FirebaseAuth.instance));
 
 final selectedAccountProvider = StateProvider<String?>((ref) => null);
 final selectedCategoryProvider = StateProvider<String?>((ref) => null);
