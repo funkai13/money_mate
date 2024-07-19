494656a33b7f079d10cf49bb0732922fa37574f3
funkai13 <evinssonmedrano@gmail.com>
2024-07-18 22:03:27 -0600

    Update income_repository.dart

--- lib/feature/Transactions/domain/repositories/income_repository.dart
+++ lib/feature/Transactions/domain/repositories/income_repository.dart
@@ -2,7 +2,25 @@ import 'package:money_mate/feature/Transactions/domain/entity/income.dart';
 
 abstract interface class IncomeRepository {
   Future<List<Income>> getAllIncomes({
-    required String uid,
-    required String financeId,
+    required String accountBalance,
+  });
+  Future<Income> createIncome({
+    required String accountBalance,
+    required String title,
+    required double amount,
+    required String category,
+    required String description,
+    required DateTime creationDate,
+    required DateTime updateDate,
+  });
+  Future<Income> getOne({
+    required String accountBalance,
+    required String currentTitle,
+    required double amount,
+  });
+  Future<void> delete({
+    required String accountBalance,
+    required String currentTitle,
+    required double amount,
   });
 }
